b11e6587340bdf30522852d356cd88ef3922851a
Rawand <rawand.faaris@gmail.com>
2023-06-25 12:01:04 +0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+let span1 = document.querySelector(".fa-heart");
+let input1 = document.querySelector(".input1");
+let num = 0
+
+span1.addEventListener("click" ,()=> {
+    input1.value= num++ ;
+});
+
+let span2 = document.querySelector(".fa-heart2");
+let input2 = document.querySelector(".input2");
+let num2 = 0
+
+span2.addEventListener("click" ,()=> {
+    input2.value= num2++ ;
+})
+
+let span3 = document.querySelector(".fa-heart3");
+let input3 = document.querySelector(".input3");
+let num3 = 0
+
+span3.addEventListener("click" ,()=> {
+    input3.value= num3++ ;
+})
+
+let span4 = document.querySelector(".fa-heart4");
+let input4 = document.querySelector(".input4");
+let num4 = 0
+
+span4.addEventListener("click" ,()=> {
+    input4.value= num4++ ;
+})
